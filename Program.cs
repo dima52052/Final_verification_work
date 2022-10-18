6ec15471d2e38e08162abb05adc6243d2396f9a1
Ershov Dmitrii <dima52052@mail.ru>
2022-10-18 16:21:38 +0500

    add  program.cs ++

--- Program.cs
+++ Program.cs
@@ -1,1 +1,38 @@
-﻿
+﻿//Обьявления массива
+string[] myArray = { "123", "1567", "-2", "435tret" };
+//Вывод массива
+string str = string.Join(", ", myArray);
+Console.Write($"[{str}] -> ");
+int j = 1;
+//Длина массива  j <=3
+for (int i = 0; i < myArray.Length; i++)
+{   
+    //Проверка ячейку массива на <=3
+    if (myArray[i].Length <= 3)
+    {        
+            
+            Console.Write($"{myArray[i]}, "); 
+            j++;
+               
+    }
+}
+//Обьявления массива меньше <=3
+string[] myArrayThree = new string[j];
+
+j=0;
+
+//Формирования нового массива
+for (int i = 0; i < myArray.Length; i++)
+{   
+    if (myArray[i].Length <= 3)
+    {        
+            
+            
+            myArrayThree[j]=myArray[i]; 
+            j++;
+               
+    }
+}
+
+string str2 = string.Join(" ", myArrayThree);
+Console.WriteLine($"Новый массив: {str2} ");
